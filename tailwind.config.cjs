2bd6b03eb38a957e92f1afceacb6d7f43d97f9ff
Tinnaphat Tin Somsang (Tinarskii) <tinvv@outlook.co.th>
2023-01-13 23:18:10 +0700

    feat: add

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx,svelte}",
+  ],  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
